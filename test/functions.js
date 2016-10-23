54f7f76b399e081ab1fa13e4cf5021fd3b276cd1
Daniel Beeke <daniel@studiofonkel.nl>
2016-10-23 21:57:32 +0200

    test for roundTo

---
+++ test/functions.js
@@ -0,0 +1,22 @@
+import test from 'ava';
+import functions from '../functions';
+import fontPresets from '../font-presets';
+
+test('getFontPreset should return a preset by name.', t => {
+    var preset = functions.getFontPreset(fontPresets, 'paragraphs');
+    t.is(preset, fontPresets['presets']['paragraphs']);
+});
+
+test('roundToNumber should round a number to another number.', t => {
+    var items = [
+        // Input, RoundTo, Proof.
+        [23, 20, 20],
+        [37, 20, 40],
+        [17, 4, 16],
+        [19, 4, 20]
+    ];
+
+    items.forEach(function (item) {
+        t.is(functions.roundToNumber(item[0], item[1]), item[2]);
+    })
+});
