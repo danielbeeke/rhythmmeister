1f899c3cccfdae513f49ec85261078da3b40f536
Daniel Beeke <daniel@studiofonkel.nl>
2016-10-23 21:58:26 +0200

    test for ceilTo

--- test/functions.js
+++ test/functions.js
@@ -20,3 +20,17 @@ test('roundToNumber should round a number to another number.', t => {
         t.is(functions.roundToNumber(item[0], item[1]), item[2]);
     })
 });
+
+test('ceilToNumber should ceil a number to another number.', t => {
+    var items = [
+        // Input, RoundTo, Proof.
+        [23, 20, 40],
+        [37, 20, 40],
+        [17, 4, 20],
+        [19, 4, 20]
+    ];
+
+    items.forEach(function (item) {
+        t.is(functions.ceilToNumber(item[0], item[1]), item[2]);
+    })
+});
