b5ad81f5b11482bf3420ea2d496b00bd43b5dda6
Daniel Beeke <daniel@studiofonkel.nl>
2016-10-23 20:49:04 +0200

    restructuring gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,34 @@
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var longhand = require('postcss-merge-longhand');
-var rhythmmeister = require('./rhythmmeister');
-var fontPresets = require('./font-presets');
+const rhythmmeister = require('./index');
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const fontPresets = require('./font-presets');
+const browserSync = require('browser-sync').create();
+const ava = require('gulp-ava');
 
-gulp.task('css', function () {
+gulp.task('serve', ['css', 'test'], () => {
+    browserSync.init({
+        server: ['./dest', './src']
+    });
+
+    gulp.watch('./src/*.html').on('change', browserSync.reload);
+    gulp.watch('./src/*.js').on('change', browserSync.reload);
+    gulp.watch(['./src/*.css'], ['css', 'test']);
+});
+
+gulp.task('css', () => {
     var processors = [
-        rhythmmeister(fontPresets),
-        longhand
+        rhythmmeister(fontPresets)
     ];
+
     return gulp.src('./src/*.css')
     .pipe(postcss(processors))
-    .pipe(gulp.dest('./dest'));
+    .pipe(gulp.dest('./dest'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('serve', ['css'], function () {
-    gulp.watch(['./src/*.css'], ['css']);
+gulp.task('test', () => {
+    return gulp.src('test/**/*.js')
+    .pipe(ava({verbose: true}));
 });
+
+gulp.task('default', ['serve']);
